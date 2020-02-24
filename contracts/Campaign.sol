7b2e4f27111d032c1d63fccf5f86570444ff5fa6
Rory Murray <rory.christian.murray@gmail.com>
2020-02-24 10:03:27 -0500

    Initialize contribute fn

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -3,12 +3,17 @@ pragma solidity ^0.4.17;
 contract Campaign {
     address public manager;
     uint public minimumContribution;
-    // address[] public approvers;
-    // Request[] public requests
+    address[] public approvers;
+
     
     function Campaign(uint _minimumContribution) public {
         manager = msg.sender;
         minimumContribution = _minimumContribution;
     }
     
-}
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        approvers.push(msg.sender)
+    }
+    
+};
