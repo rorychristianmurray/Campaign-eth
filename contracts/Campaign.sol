5995cb2c44f283c9a5faa0c0dcebaf710f14d180
Rory Murray <rory.christian.murray@gmail.com>
2020-02-27 17:44:00 -0500

    Create request struct

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,6 +1,14 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
+    // a struct introduces a new type
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+    
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
